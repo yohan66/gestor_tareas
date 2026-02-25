50021ac24e0f2ed5b24fb1f2f2ba64d676a70580
yohan rosales <rosalesyohan@gmail.com>
2026-02-25 19:23:19 -0400

    Menu completado

--- main.py
+++ main.py
@@ -41,3 +41,64 @@ def completar_tarea(tareas, id_tarea):
             return True # Retorna True si la encontró y modificó
     return False
 
+def menu():
+    """Interfaz de consola para el usuario."""
+    tareas = cargar_tareas()
+    
+    while True:
+        print("\n--- GESTOR DE TAREAS ---\n")
+        print("1. Agregar nueva tarea")
+        print("2. Ver tareas pendientes")
+        print("3. Marcar tarea como completada")
+        print("4. Salir\n")
+        opcion = input("Elige una opción: ")
+
+        if opcion == '1':
+            titulo = input("Título: ")
+            desc = input("Descripción: ")
+            fecha = input("Fecha límite (DD/MM/AAAA): ")
+            tareas = agregar_tarea(tareas, titulo, desc, fecha)
+            guardar_tareas(tareas)
+            print("\nTarea agregada con éxito.")
+            
+        elif opcion == '2':
+            pendientes = listar_pendientes(tareas)
+            print("\n--- TAREAS PENDIENTES ---")
+            if not pendientes:
+                print("No hay tareas pendientes. ¡Buen trabajo!")
+            for t in pendientes:
+                print(f"[{t['id']}] {t['titulo']} (Vence: {t['fecha']}) - {t['descripcion']}")
+                
+        elif opcion == '3':
+            pendientes = listar_pendientes(tareas)
+            
+            #Verificamos si hay tareas por completar
+            if not pendientes:
+                print("No hay tareas pendientes. ¡Buen trabajo!")
+            else:
+                print("\n--- Tareas Disponibles para Completar ---")
+                # Mostramos un resumen rapido con ID y el titulo
+                for t in pendientes:
+                    print(f"[{t['id']}] {t['titulo']}")
+                    
+                id_tarea: str = input("\nIngresa el ID de la tarea a completar (o escribe 'salir' para cancelar): ")
+                
+                # Opcion para salir por si el usuario desea
+                
+                if id_tarea.lower() == 'salir':
+                    print("Operacion Cancelada")
+                else:
+                    if completar_tarea(tareas,id_tarea):
+                        guardar_tareas(tareas)
+                        print("Tarea Marcada como Completada")
+                    else:
+                        print("Tarea no Encontrada, Revisa El ID de la Tarea e intentalo denuevo")
+                    
+        elif opcion == '4':
+            print("\n¡Hasta luego!")
+            break
+        else:
+            print("Opción no válida.")
+
+if __name__ == "__main__":
+    menu()
